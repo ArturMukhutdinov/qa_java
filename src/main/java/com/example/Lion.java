035e08717c157a30b9747b1a210aeaec515572af
Artur Mukhutdinov <muhutdinov.artur@yandex.ru>
2021-10-31 20:51:42 +0300

    commit1

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -6,17 +6,19 @@ public class Lion {
 
     boolean hasMane;
 
-    public Lion(String sex) throws Exception {
+    private Feline feline;
+
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
 
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
